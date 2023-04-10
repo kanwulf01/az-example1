93348bfe57d4e60d28b735704cff627072b41557
DESKTOP-QT912V3\Christian D. Millan <radamanthyzdark@hotmail.com>
2023-04-10 11:14:34 -0500

    add print in feature

--- example1/__init__.py
+++ example1/__init__.py
@@ -5,7 +5,7 @@ import azure.functions as func
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-
+    print("HOLA CHRISTIAN")
     name = req.params.get('name')
     if not name:
         try:
