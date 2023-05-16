d60552032c445da3eccc9c67cb0750acd74fea85
DESKTOP-QT912V3\Christian D. Millan <radamanthyzdark@hotmail.com>
2023-05-16 15:12:15 -0500

    2 c

--- example1/__init__.py
+++ example1/__init__.py
@@ -1,24 +1,27 @@
 import logging
 
 import azure.functions as func
-
+import json
+from src.error import Errors
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
 
-    name = req.params.get('name')
-    if not name:
-        try:
-            req_body = req.get_json()
-        except ValueError:
-            pass
-        else:
-            name = req_body.get('name')
+    try:
+        body = req.get_json()
 
-    if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
-    else:
+        if body == None:
+            raise ValueError()
+    
         return func.HttpResponse(
              "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
              status_code=200
+        )
+    except Exception as err:
+        e = Errors(500, None, str(err))
+        return func.HttpResponse(
+             #body=json.dumps({"error":str(err)}),
+             body=e.response(),
+             status_code=200,
+              mimetype='application/json'
         )
---
+++ example1/src/error.py
@@ -0,0 +1,21 @@
+
+
+from typing_extensions import Self
+import json
+
+class Errors():
+
+
+    def __init__(self, status, data, error):
+
+        self.status = status
+        self.data   = data
+        self.error  = error
+
+    def response(self):
+        res = {
+            "OK":self.status,
+            "data":self.data,
+            "error":self.error
+        }
+        return json.dumps(res) 
