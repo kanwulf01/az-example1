6d4dd8c8a382b11b9b55c19a35a3e542c077380f
DESKTOP-QT912V3\Christian D. Millan <radamanthyzdark@hotmail.com>
2023-05-16 15:35:32 -0500

    test

--- example1/__init__.py
+++ example1/__init__.py
@@ -6,7 +6,7 @@ from src.error import Errors
 
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-    print("HOLA CHRISTIAN")
+    
     name = req.params.get('name')
     if not name:
         try:
